c292a42b4dcfe9fdbf5b10f1b6a6d21649e08299
Jerónimo Zapata <156379009+JeronimoZapata@users.noreply.github.com>
2025-04-18 01:48:21 -0300

    Update carmichael.py

--- TPI COMPLEJIDAD/carmichael.py
+++ TPI COMPLEJIDAD/carmichael.py
@@ -1,8 +1,26 @@
 import mcd
 import fermat
+import prime
 
 def carmichel(x, y):
 
-    for num in range(x, y):
+    ls_carmichael = []
+    for num in range(x, (y + 1)):
 
-        return
+        ls_coprim = []
+        for element in range(1, num):
+
+            if mcd.mcd(element, num) == 1:
+                ls_coprim.append(element)
+        
+        flag = True
+        for coprim in ls_coprim:
+            
+            if not (fermat.fermat(coprim, num)):
+                flag = False
+                break
+        
+        if flag and (not (prime.prime(num))):
+            ls_carmichael.append(num)
+
+    return ls_carmichael
