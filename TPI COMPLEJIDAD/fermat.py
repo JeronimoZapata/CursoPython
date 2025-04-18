4b33e66cdf92113626177396afbfaf96161ea486
Jerónimo Zapata <156379009+JeronimoZapata@users.noreply.github.com>
2025-04-18 01:47:43 -0300

    Update fermat.py

--- TPI COMPLEJIDAD/fermat.py
+++ TPI COMPLEJIDAD/fermat.py
@@ -1,12 +1,9 @@
 def fermat(a, n):
 
-    rest = (a ** (n -1)) % n
-    
-    if rest == 1: 
+    if ((a ** (n - 1)) % n) == 1:
         return True
-    else: 
+    else:
         return False
-    
 
 '''
 resultado = fermat(2, 9)
