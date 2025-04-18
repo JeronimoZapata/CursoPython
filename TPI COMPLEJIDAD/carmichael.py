4f8eea4f7a55653a16468d088cf54a8a49fd8d77
Jerónimo Zapata <156379009+JeronimoZapata@users.noreply.github.com>
2025-04-18 18:12:12 -0300

    Update carmichael.py

--- TPI COMPLEJIDAD/carmichael.py
+++ TPI COMPLEJIDAD/carmichael.py
@@ -7,20 +7,21 @@ def carmichel(x, y):
     ls_carmichael = []
     for num in range(x, (y + 1)):
 
-        ls_coprim = []
-        for element in range(1, num):
-
-            if mcd.mcd(element, num) == 1:
-                ls_coprim.append(element)
-        
-        flag = True
-        for coprim in ls_coprim:
+        if not (prime.prime(num)):
+            ls_coprim = []
+            for element in range(1, num):
+    
+                if mcd.mcd(element, num) == 1:
+                    ls_coprim.append(element)
             
-            if not (fermat.fermat(coprim, num)):
-                flag = False
-                break
-        
-        if flag and (not (prime.prime(num))):
-            ls_carmichael.append(num)
+            flag = True
+            for coprim in ls_coprim:
+                
+                if not (fermat.fermat(coprim, num)):
+                    flag = False
+                    break
+            
+            if flag:
+                ls_carmichael.append(num)
 
     return ls_carmichael
