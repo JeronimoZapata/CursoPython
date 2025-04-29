4299392cffec2b872bce7e4449e60a22f99fc091
Wilmake-sys <matiaselima@hotmail.com>
2025-04-29 16:27:07 -0300

    Add files via upload

--- carmichael.py
+++ carmichael.py
@@ -1,28 +1,30 @@
-import mcd
 import fermat
+import SchönhageStrassen
+import cribaerastotenes
 import prime
+import mcd
 
 def carmichel(x, y):
 
     ls_carmichael = []
-    lista_prim = prime.is_prime_sieve (y) #genera una lista de todos los numeros primos hasta ese valor (gonza)
+    list_prim = prime.is_prime_sieve(y)
+
     for num in range(x, (y + 1)):
 
-        if num not in set(lista_prim): #set es otro tipo de estructura, donde pregunta directamente por la clave (gonza)
+        if not (list_prim[num]): 
 
             for element in range(1, num):                          #An3 + Bn2 + C
-                
+
                 flag = True
                 if mcd.mcd(element, num) == 1:      #Lo que mas tarda
-                    
-                    if not (fermat.fermat(element, num)):
+
+                    if fermat.fermat(element, num-1, num) !=1:
                         flag = False
                         break
 
             if flag:
-                #print(num)
                 ls_carmichael.append(num)
 
     return ls_carmichael
 
-print(carmichel(1, 562))
+print(carmichel(1, 1000000))
--- fermat.py
+++ fermat.py
@@ -1,10 +1,20 @@
-def fermat(a, n):
+def fermat_malo(a, n):
 
     if ((a ** (n - 1)) % n) == 1:
         return True
     else:
         return False
 
+def fermat(a, e, n):
+    # Calcula a^e mod n usando exponenciación rápida
+    resultado = 1
+    a = a % n
+    while e > 0:
+        if e % 2 == 1:
+            resultado = (resultado * a) % n
+        a = (a * a) % n
+        e //= 2
+    return resultado
 
 '''
 resultado = fermat(2, 9)
