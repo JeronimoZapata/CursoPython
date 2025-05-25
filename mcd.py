855d6e905ec0e981e39e2772bf6876fafcfe8957
Alejo <alejoivanrojas@gmail.com>
2025-05-24 22:13:19 -0300

    refactor mcd function for clarity and efficiency

--- mcd.py
+++ mcd.py
@@ -1,7 +1,4 @@
 def mcd(a, b):
-    while b != 0:
+    while b:
         a, b = b, a % b 
-        # la linea 3 la hace en el mismo instante de la manera que esta representado abajo, pero no se cambia el valor de A al dividir
-        # a = b
-        # b = a % b
-    return a
+    return abs(a)
